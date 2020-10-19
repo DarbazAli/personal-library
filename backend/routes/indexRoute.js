697bce99d3b2c74f550bd9e7fb8958bb6933e542
Darbaz Ali <darbaz.me@gmail.com>
2020-10-19 10:09:58 +0300

    create backend

--- backend/routes/indexRoute.js
+++ backend/routes/indexRoute.js
@@ -2,7 +2,8 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Personal Library' })
+    // res.render('index', { title: 'Personal Library' })
+    res.send('API working...')
 })
 
 export default router
