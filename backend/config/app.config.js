d6c5a7b52357acfc752db9c92f6196ccbffd1693
Darbaz Ali <darbaz.me@gmail.com>
2020-10-21 23:04:03 +0300

    update security settings

--- backend/config/app.config.js
+++ backend/config/app.config.js
@@ -12,7 +12,16 @@ const configApp = (app) => {
     app.use(express.static(process.cwd() + '/public'))
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(bodyParser.json())
+
+    // security
     app.use(helmet.xssFilter())
+    app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }))
+    app.use(helmet.frameguard({ action: 'deny' }))
+    app.use(helmet.noSniff())
+    app.use(helmet.ieNoOpen())
+    app.use(helmet.hsts())
+
+    // method override
     app.use(methodOverride('_method'))
 }
 
